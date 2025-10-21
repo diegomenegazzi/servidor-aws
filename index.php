5fead576a9ed13cdf4d9da6d235239a658bdb8c4
Diego Menegazzi <omenegazzi@gmail.com>
2025-10-21 20:47:12 -0300

    Criado variavel nome e exibido em tela.

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 </head>
 <body>
     <?php
+        $nome = "Diego Menegazzi";
         echo "Hoje é dia " . date("dd/mm/yyyy") . "e agora são: " . date("HH:ii:ss");
+        echo "<h1> Meu nome é: $nome </h1>"; 
     ?>        
 </body>
 </html>
