05714043449b12a0d004f6c1e5e3d57e29944493
Diego Menegazzi <omenegazzi@gmail.com>
2025-10-22 19:47:46 -0300

    Corrigido ponto e virgula que estavam faltando.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <?php
         $nome = "Diego Menegazzi";
         $idade = "35";
-        $cidade = "Treze Tílias"
+        $cidade = "Treze Tílias";
         echo "Hoje é dia " . date("dd/mm/yyyy") . "e agora são: " . date("HH:ii:ss");
         echo "<h1> Meu nome é: $nome </h1>";
         echo "<h1> Minha idade é: $idade </h1>";
