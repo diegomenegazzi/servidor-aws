ad7ce9ccfb4b257c4ecf3d2e592c3cdd9ffbaa27
Diego Menegazzi <omenegazzi@gmail.com>
2025-10-22 22:29:52 -0300

    Retirado campos.

--- index.php
+++ index.php
@@ -10,14 +10,10 @@
         $nome = "Diego Menegazzi";
         $idade = "35";
         $cidade = "Treze Tílias";
-        $telefone = "0000";
-        $email = "diego.menegazzi@prof.sc.senac.br"
         echo "Hoje é dia " . date("dd/mm/yyyy") . "e agora são: " . date("HH:ii:ss");
         echo "<h1> Meu nome é: $nome </h1>";
         echo "<h1> Minha idade é: $idade </h1>";
         echo "<h1> Minha cidade é: $cidade </h1>";
-        echo "<h1> Meu telefone é: $telefone </h1>";
-        echo "<h1> Meu E-mail é: $email </h1>";
     ?>        
 </body>
 </html>
